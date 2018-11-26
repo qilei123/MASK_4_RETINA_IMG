4451bcd2506370363f8756c8affa62c3120f36d9
qilei123 <qileimail123@gmail.com>
2018-11-26 13:17:22 -0500

    daf

--- scripts/train_alternate_ROP.sh
+++ scripts/train_alternate_ROP.sh
@@ -3,7 +3,7 @@ export PYTHONUNBUFFERED=1
 export MXNET_ENABLE_GPU_P2P=0
 export PYTHONPATH=${PYTHONPATH}:../MASK_4_RETINA_IMG/incubator-mxnet/python/
 
-TRAIN_DIR=/home/qileimail123/data0/RetinaImg/ROP_COCO/output/baseline2/
+TRAIN_DIR=/home/qileimail123/data0/RetinaImg/ROP_COCO/output/baseline2
 DATASET=coco
 SET=train2014
 TEST_SET=val2014
