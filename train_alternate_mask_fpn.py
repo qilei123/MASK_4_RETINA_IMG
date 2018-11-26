614b4cdaae927899e840a09568ad1a3140fd73ec
qilei123 <qileimail123@gmail.com>
2018-11-26 13:15:58 -0500

    fdad

--- train_alternate_mask_fpn.py
+++ train_alternate_mask_fpn.py
@@ -23,14 +23,13 @@ def alternate_train(args, ctx, pretrained, epoch,
 
     # model path
     model_path = args.prefix
-
+    '''
     logging.info('########## TRAIN RPN WITH IMAGENET INIT')
-    print args.resume
     train_rpn(args.network, args.dataset, args.image_set, args.root_path, args.dataset_path,
               args.frequent, args.kvstore, args.work_load_list, args.no_flip, args.no_shuffle, args.resume,
               ctx, pretrained, epoch, model_path+'/rpn1', begin_epoch, rpn_epoch,
               train_shared=False, lr=rpn_lr, lr_step=rpn_lr_step)
-    '''
+    
     logging.info('########## GENERATE RPN DETECTION')
     image_sets = [iset for iset in args.image_set.split('+')]
     for image_set in image_sets:
